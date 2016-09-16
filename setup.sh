c4e6951e4988ffd33955729f1c8ab9ca0b81e17b
Peter H <peter.steffensen@gmail.com>
2016-09-16 21:17:42 +0200

    added setup script and new improved vim config. Note the setup script is to be run on ubuntu

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get install curl
+
+sudo apt-get install vim-python-jedi
+vim-addons  install python-jedi
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+
+cp .vimrc ~/.vimrc
