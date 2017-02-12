36a9ef22d75e9371ca6618eec6afdc8cb460314e
hoeg <peter.steffensen@gmail.com>
2017-02-12 19:33:32 +0100

    added zsh and python folding plugin to vim

--- setup.sh
+++ setup.sh
@@ -5,11 +5,20 @@ sudo apt-get install curl
 sudo apt-get install vim-python-jedi
 vim-addons install python-jedi
 
+# vim setup
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+git submodule add https://github.com/tmhedberg/SimpylFold.git ~/.vim/bundle/SimpylFold
+pushd ~/.vim
+git submodule init
+popd
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $DIR/.vimrc ~/.vimrc
+
+#zsh
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
